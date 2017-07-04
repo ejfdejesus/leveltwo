8ed4fca1220092744fc43a8bf8638ef911ab2b1a
Clarence Tan <clarenciotan@gmail.com>
2017-07-04 11:13:15 +0200

    just added some grouping and data provider stuff in TestNG

--- src/main/java/com/clarence/tests/BasicTestsToUse.java
+++ src/main/java/com/clarence/tests/BasicTestsToUse.java
@@ -1,10 +1,19 @@
 package com.clarence.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class BasicTestsToUse {
 
+	@DataProvider(name = "test1")
+	public Object[][] createData1() {
+		return new Object[][]{
+			{"Cedric", new Integer(36)},
+			{"Anne", new Integer(37)}
+		};
+	}
+	
 	private String PrintMe()
 	{
 		return "Sample from PrintMe()";
@@ -15,7 +24,7 @@ public class BasicTestsToUse {
 		return "Your name should be: "+ name;
 	}
 	
-	@Test
+	@Test(groups = {"chika"})
 	public void TestOne()
 	{
 		Assert.assertEquals("Sample from PrintMe()", PrintMe());
@@ -24,7 +33,13 @@ public class BasicTestsToUse {
 	@Test
 	public void TestTwo()
 	{
-		Assert.assertEquals("Your name should be: Bike", AnotherPrinter("Mike"));
+		Assert.assertEquals("Your name should be: Mike", AnotherPrinter("Mike"));
+	}
+	
+	@Test(groups= {"chika"}, dataProvider = "test1")
+	public void TestThree(String name, int age)
+	{
+		System.out.println(name + " " + age);
 	}
 
 }
